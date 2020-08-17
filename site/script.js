f002c6bd56b1be208172622c8e900ed19f3fbaac
abhilekh <abhilekhgautam119@gmail.com>
2020-08-17 21:33:28 +0545

    files

---
+++ site/script.js
@@ -0,0 +1,17 @@
+setInterval(setClock,1000);
+const secondHand=document.querySelector(".second");
+const minuteHand=document.querySelector(".minute");
+const hourHand=document.querySelector(".hour");
+function setClock(){
+	const currentDate=new Date();
+	const secondRatio=currentDate.getSeconds()/60;
+	const minuteRatio=(secondRatio+currentDate.getMinutes())/60;
+	const hourRatio=(minuteRatio+currentDate.getHours())/12;
+	setRotation(secondHand,secondRatio);
+	setRotation(minuteHand,minuteRatio);
+	setRotation(hourHand,hourRatio);
+}
+
+function setRotation(element,rotationRatio){
+	element.style.setProperty('--rotation',rotationRatio* 360);
+}
